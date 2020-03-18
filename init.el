3fc16e52280a049d58b95cd69db0005fa2d42bc7
Gearnode <bryan@frimin.fr>
2020-03-18 23:04:44 +0100

    update init.el

--- init.el
+++ init.el
@@ -1,6 +1,7 @@
 ;;; -*- lexical-binding: t -*-
 (require 'seq)
 
+
 (setq package-user-dir
       (expand-file-name "packages"
                         user-emacs-directory))
@@ -25,7 +26,7 @@
  '(custom-enabled-themes (quote (tango-dark)))
  '(package-selected-packages
    (quote
-    (dockerfile-mode yasnippet ztree company-quickhelp exec-path-from-shell terraform-mode helm-ag json-mode forge elixir-mode erlang disable-mouse company-go company protobuf-mode yaml-mode markdown-mode go-mode org-plus-contrib org magit helm))))
+    (nginx-mode dockerfile-mode yasnippet ztree company-quickhelp exec-path-from-shell terraform-mode helm-ag json-mode forge elixir-mode erlang disable-mouse company-go company protobuf-mode yaml-mode markdown-mode go-mode org-plus-contrib org magit helm))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
